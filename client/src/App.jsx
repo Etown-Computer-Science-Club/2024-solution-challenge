d4858a1adb627a80653944d43df7ca1999cc0e44
Ethan Lajeunesse <ethan@lajfam.com>
2024-02-01 16:10:52 -0500

    inital repo setup with tailwind and antd

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { Button } from "antd";
+import "./App.css";
+
+function App() {
+	return (
+		<div className="App">
+			<Button type="primary" className="w-full">
+				Button
+			</Button>
+		</div>
+	);
+}
+
+export default App;
